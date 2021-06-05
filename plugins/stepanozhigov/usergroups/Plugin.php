11f7b20a86fc17a8c00e8d37786c5f542564aa50
stepanozhigov <stepan.ozhigov@gmail.com>
2021-06-06 00:33:32 +0200

    groups column added to user table

--- plugins/stepanozhigov/usergroups/Plugin.php
+++ plugins/stepanozhigov/usergroups/Plugin.php
@@ -1,14 +1,36 @@
 <?php namespace StepanOzhigov\Usergroups;
 
+use phpDocumentor\Reflection\Type;
 use System\Classes\PluginBase;
+use RainLab\User\Controllers\Users as UsersController;
+use RainLab\User\Models\User as UserModel;
 
 class Plugin extends PluginBase
 {
+
+    public $require = ['RainLAb.User'];
+
     public function registerComponents()
     {
     }
 
     public function registerSettings()
     {
+    }
+
+    public function boot() {
+        UsersController::extendListColumns(function($list,$model) {
+            if(!$model instanceof UserModel) return;
+
+            $list->addColumns([
+                'group_name'=>[
+                    'label' => 'Groups',
+                    'relation' => 'groups',
+                    'select' => 'name'
+                ]
+            ]);
+
+            // dd($list);
+        });
     }
 }
